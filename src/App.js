44a44369467d3124e82a2142e99de29b304bd300
malhibilal <malhibilal@gmail.com>
2023-04-11 13:59:19 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import header from './component/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <header />
+    
+      <h1> this is my react project</h1>
+      <p> this is first paragraph</p>
+      <header />
+      <hr/>
     </div>
   );
 }
---
+++ src/component/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function header(){
+    return(
+        <div>
+            <h1>this is a header</h1>
+            <p>this is a paragraph for learning purpose</p>
+        </div>
+    );
+}
+export default header;
